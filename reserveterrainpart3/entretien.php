3d2e5d0e6f8e0239e20294cb01064ccdc1cf4f7b
Aminech2004 <proufisure123@gmail.com>
2024-11-30 02:01:01 +0100

    Update entretien.php
    modification dyali (amine chalabi)

--- reserveterrainpart3/entretien.php
+++ reserveterrainpart3/entretien.php
@@ -5,18 +5,22 @@ include 'config.php'; // Inclusion du fichier de connexion
 $query = "SELECT idTerrain, etat FROM TERRAINS WHERE etat IN (1, 2)";
 $result = mysqli_query($conn, $query);
 
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn)); // Error handling
+}
+
 if (mysqli_num_rows($result) > 0) {
     echo "<h2>Liste des terrains en entretien ou hors service</h2>";
     echo "<table border='1'>
-            <tr>
-                <th>ID Terrain</th>
-                <th>État</th>
-            </tr>";
+    <tr>
+    <th scope='col'>ID Terrain</th>
+    <th scope='col'>État</th>
+    </tr>";
     while ($row = mysqli_fetch_assoc($result)) {
         echo "<tr>
-                <td>" . $row['idTerrain'] . "</td>
-                <td>" . ($row['etat'] == 1 ? 'En entretien' : 'Hors service') . "</td>
-              </tr>";
+        <td>" . $row['idTerrain'] . "</td>
+        <td>" . ($row['etat'] == 1 ? 'En entretien' : 'Hors service') . "</td>
+        </tr>";
     }
     echo "</table>";
 } else {
